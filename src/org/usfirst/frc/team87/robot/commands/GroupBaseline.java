3e52191c0ae870ceb2fa75b0b3a55a8adb93d08d
Aidan Mundy <amboyscout@aol.com>
2017-03-18 05:04:45 -0400

    Baseline auto done?

--- src/org/usfirst/frc/team87/robot/commands/GroupBaseline.java
+++ src/org/usfirst/frc/team87/robot/commands/GroupBaseline.java
@@ -6,25 +6,11 @@ import org.usfirst.frc.team87.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class GroupBaseline extends CommandGroup {
-	int sideMultiplier = -1+(2*RobotMap.startingSide);
-    public GroupBaseline() {
-    	requires(Robot.drivebase);
-    	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    }
+	public GroupBaseline() {
+		if (RobotMap.startingPosition != 1) {
+			addSequential(new AutoDrive(-152.812));
+		} else if (RobotMap.startingPosition == 1) {
+			addSequential(new AutoDrive(-80.708));
+		}
+	}
 }
