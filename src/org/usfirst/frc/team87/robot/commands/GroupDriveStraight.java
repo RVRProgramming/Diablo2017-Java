36f8841f3ea096acb2cfcb69cf81e3508876986d
RVR87Programming <programming@rvr87.com>
2017-05-08 16:04:45 -0400

    Auto timing changes

--- src/org/usfirst/frc/team87/robot/commands/GroupDriveStraight.java
+++ src/org/usfirst/frc/team87/robot/commands/GroupDriveStraight.java
@@ -3,9 +3,7 @@ package org.usfirst.frc.team87.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
-/**
- *
- */
+
 public class GroupDriveStraight extends CommandGroup {
 
     public GroupDriveStraight() {
