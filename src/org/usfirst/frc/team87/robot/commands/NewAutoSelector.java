eee3bcde60a7fbbb974a3f2fe5fd1cab71e32323
RVR87Programming <programming@rvr87.com>
2017-06-14 16:45:47 -0400

    More stuff

--- src/org/usfirst/frc/team87/robot/commands/NewAutoSelector.java
+++ src/org/usfirst/frc/team87/robot/commands/NewAutoSelector.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team87.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import newSelector.Selector;
+import newSelector.Selector.inputType;
+import newSelector.Selector.rowInteraction;
 
 /**
  *
@@ -10,12 +12,18 @@ public class NewAutoSelector extends Command {
 	Selector newSelector;
 
 	public NewAutoSelector() {
-		newSelector = new Selector(;)
+		int[] inputs = {0};
+		newSelector = new Selector(2, rowInteraction.off, inputType.dPad, inputs);
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-
+		String[] row1options={"1", "2", "3"};
+		String[] row2options={"1","2"};
+		newSelector.setRowName(0, "Row 1");
+		newSelector.setRowName(1, "Row 2");
+		newSelector.setRowOptions(0, row2options);
+		newSelector.setRowOptions(1, row2options);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
